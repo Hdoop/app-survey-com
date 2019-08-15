5cee3251c0d0cc34ce9224566da9996c9ddd4d65
Gaurav <gauravarora3208@gmail.com>
2019-08-15 17:42:05 +0530

    login page design integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { BrowserModule }                from '@angular/platform-browser';
-import { ReactiveFormsModule }          from '@angular/forms';
-import { NgModule }                     from '@angular/core';
-import { AppComponent }                 from './app.component';
-import { DynamicFormComponent }         from './dynamic-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form.component';
 import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, DynamicFormComponent, DynamicFormQuestionComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent,
+    LoginComponent
+  ],
+  // bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule {
   constructor() {
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+    selector: 'login-component',
+    templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+    constructor() {
+        
+    }
+
+}
